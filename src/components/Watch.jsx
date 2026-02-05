64c3f1234e4447ef31fcd6ab44f655230c388c5e
Arif sherasiya <arifsherasiya78692@gmail.com>
2026-02-05 09:25:29 +0530

    first commit

---
+++ src/components/Watch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+export const Watch = () => {
+    const movieName = useParams().name
+    return (
+        <div>
+            <h1>WATCHING....{movieName}</h1>
+        </div>
+    )
+}
